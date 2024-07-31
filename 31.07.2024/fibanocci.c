672f4264a9cd22110f35ea1da16b83a6b75241a9
Dharsinijayaprakash <151325936+Dharsinijayaprakash@users.noreply.github.com>
2024-07-31 22:17:23 +0530

    Create fibanocci.c

---
+++ 31.07.2024/fibanocci.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+    int main(){
+        int u,x=0,y=1,n;
+        printf("Enter the number of terms: ");
+        scanf("%d",&u);
+        printf("Fibonacci Sequence: ");
+        for(int i=1;i<=u;++i){
+            printf("%d ",t1);
+            n=x+y;
+            x=y;
+            y=n;
+        }
+        printf("\n");
+
+    }
